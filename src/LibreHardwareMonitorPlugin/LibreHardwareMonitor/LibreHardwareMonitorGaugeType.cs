daa721cb001c98e2ba1ee62c71d4b6c0036b9587
Changmin <changmin811@gmail.com>
2025-01-06 02:44:30 +0900

    Update LibreHardwareMonitorGaugeType.cs

--- src/LibreHardwareMonitorPlugin/LibreHardwareMonitor/LibreHardwareMonitorGaugeType.cs
+++ src/LibreHardwareMonitorPlugin/LibreHardwareMonitor/LibreHardwareMonitorGaugeType.cs
@@ -7,21 +7,30 @@
         CPUPower = 2,
         CPUCore = 3,
         CPUPackage = 4,
-        GPULoad = 5,
-        GPUPower = 6,
-        GPUCore = 7,
-        GPUHotspot = 8,
-        Memory = 9,
-        RAM= 10,
-        VrMemory = 11,
-        VrRAM = 12,
+        CPUMonitor = 5,
+
+        GPULoad = 6,
+        GPUPower = 7,
+        GPUCore = 8,
+        GPUHotspot = 9,
+        GPUMonitor = 10,
+
+        Memory = 11,
+        VrMemory = 12,
         GPUMemory = 13,
-        VRAM = 14,
-        CPUMonitor = 15,
-        GPUMonitor = 16,
-        MEMMonitor = 17,
+        MEMMonitor = 14,
+
+        RAM = 15,
+        VrRAM = 16,
+        VRAM = 17,
         RAMMonitor = 18,
-        Battery = 19,
-        Count = 20 // should always be the last one
+
+        NVME1 = 19,
+        NVME2 = 20,
+        NVME3 = 21,
+
+        Battery = 22,
+        
+        Count = 23 // should always be the last one
     }
 }
