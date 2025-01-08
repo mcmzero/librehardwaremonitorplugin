8ba25f5d9d8683cbe1367b9b3c0619d527d5eccc
Changmin <changmin811@gmail.com>
2025-01-08 13:08:19 +0900

    Update BuildPackage.cmd

--- src/LibreHardwareMonitorPlugin/BuildEvents/BuildPackage.cmd
+++ src/LibreHardwareMonitorPlugin/BuildEvents/BuildPackage.cmd
@@ -6,3 +6,5 @@ set OUTPUT_DIR=%~2
 xcopy /s /y %PROJECT_DIR%metadata\ %OUTPUT_DIR%metadata\
 
 %PROJECT_DIR%LoupedeckPluginTool\LoupedeckPluginTool.exe pack -input=%OUTPUT_DIR% -output=%OUTPUT_DIR%..\LibreHardwareMonitor.lplug4
+
+LogiPluginTool.exe install -path=%OUTPUT_DIR%..\LibreHardwareMonitor.lplug4
